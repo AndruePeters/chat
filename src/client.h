776dcc784ffe94d2958fb140c0ba2bcd68efe62b
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2021-04-28 10:31:14 -0400

    added user

--- src/client.h
+++ src/client.h
@@ -4,6 +4,16 @@
 
 #ifndef BEAST_CLIENT_H
 #define BEAST_CLIENT_H
+
 #include "message.h"
+
+#include <string_view>
+
+/// This version of a chat user only has a sender string and can send a message from the sender
+class ChatUser {
+    std::string sender = "anonymous";
+public:
+    void sendMessage(std::string_view message);
+};
 
 #endif //BEAST_CLIENT_H
