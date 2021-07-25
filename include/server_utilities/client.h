592d928727601959ac93eddcd49ed1781d7db431
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2021-07-24 21:06:03 -0400

    chat_client_cli now sends multiple messages, keeping the session alive.

--- include/server_utilities/client.h
+++ include/server_utilities/client.h
@@ -43,94 +43,37 @@ class session : public std::enable_shared_from_this<session>
 
   public:
     explicit session(net::io_context& ioc);
-
     ~session() = default;
 
-    template<class Body, class Allocator>
-    void run(http::request<Body, http::basic_fields<Allocator>> req);
-
-    void send(const std::shared_ptr<const std::string>& ss);
-
+    /// Sets the host for this session
     void setHost(std::string host);
+
+    /// Sets the port for this session
     void setPort(std::string port);
 
+    /// Connect to the server
     void connect();
+
+    /// Send the message
+    /// This should ultimately be changed to accept a Message object
+    /// Or a JSON object. Not sure at the moment
+    void send(const std::shared_ptr<const std::string>& ss);
 
   private:
     void on_send(const std::shared_ptr<const std::string>& ss);
-
-    void on_accept(beast::error_code ec);
 
     void on_read(beast::error_code ec, std::size_t bytesTransferred);
 
     void on_write(beast::error_code ec, std::size_t bytesTransferred);
 
-    void on_resolve(beast::error_code ec, tcp::resolver::results_type results)
-    {
-      if (ec) return fail(ec, "resolve");
+    void on_resolve(beast::error_code ec, tcp::resolver::results_type results);
 
-      beast::get_lowest_layer(ws).expires_after(std::chrono::seconds(30));
+    /// Get's called by on_resolve() after on_resolve()
+    /// Need to change the name of this function or the public facing connect()
+    void on_connect(beast::error_code ec, tcp::resolver::results_type::endpoint_type ep);
 
-      beast::get_lowest_layer(ws).async_connect(
-        results,
-        beast::bind_front_handler(&session::on_connect, shared_from_this()) );
-
-    }
-
-    void on_connect(beast::error_code ec, tcp::resolver::results_type::endpoint_type ep)
-    {
-        if (ec) return fail(ec, "connect");
-
-      // turn off the timeout on the tcp_stream because
-      // the websocket stream has its own timeout system
-      beast::get_lowest_layer(ws).expires_never();
-
-      // set suggested timeout settings for the websocket
-      ws.set_option(websocket::stream_base::timeout::suggested(beast::role_type::client));
-
-      // set a decorator to change the User-Agent of the handshake
-      ws.set_option(websocket::stream_base::decorator(
-              [](websocket::request_type& req) {
-                  req.set(http::field::user_agent, std::string(BOOST_BEAST_VERSION_STRING) + " chat-client");
-              }));
-
-      // update the host string. This will provide the value of the
-      // host HTTP header during the websocket handshake
-      // the guide references: https://tools.ietf.org/html/rfc7230#section-5.4
-      host += ':' + std::to_string(ep.port());
-
-      // now do the handshake
-      ws.async_handshake(host, "/", beast::bind_front_handler(&session::on_handshake, shared_from_this()));
-    }
-
-    void on_handshake(beast::error_code ec)
-    {
-      if (ec) return fail(ec, "handshake");
-      spdlog::info("Successfully completed handshake.");
-    }
+    void on_handshake(beast::error_code ec);
 };
 
-template<class Body, class Allocator>
-void session::run(http::request<Body, http::basic_fields<Allocator>> req)
-{
-    // Set suggested timeout settings for the websocket
-    ws.set_option(
-      websocket::stream_base::timeout::suggested(
-        beast::role_type::client));
-
-    // Set a decorator to change the Server of the handshake
-    /*ws.set_option(websocket::stream_base::decorator(
-      [](websocket::response_type& res) {
-          res.set(http::field::client,
-            std::string(BOOST_BEAST_VERSION_STRING) + " websocket-chat-multi");
-      })); */
-
-    // Accept the websocket handshake
-    ws.async_accept(
-      req,
-      beast::bind_front_handler(
-        &session::on_accept,
-        shared_from_this()));
-}
 } /// Client
 #endif //CHAT_CLIENT_H
--- src/client.cpp
+++ src/client.cpp
@@ -5,7 +5,6 @@
 #include <chrono>
 #include <thread>
 
-#include <spdlog/spdlog.h>
 
 #include <spdlog/spdlog.h>
 
@@ -25,68 +24,11 @@ session::session(net::io_context& ioc)
 {
 }
 
-void session::on_accept(beast::error_code ec)
+void session::connect()
 {
-    if (ec) return fail(ec, "accept");
-
-    ws.async_read(buffer, beast::bind_front_handler(&session::on_read, shared_from_this()));
+    resolver.async_resolve(this->host, this->port, beast::bind_front_handler(&session::on_resolve, shared_from_this()));
 }
 
-
-void session::on_read(beast::error_code ec, std::size_t)
-{
-    if (ec) return fail(ec, "read");
-
-    // send to all connections
-    const std::string msg = beast::buffers_to_string(buffer.data());
-    //ws.send(std::make_shared<const std::string>(msg));
-    spdlog::info("Recieved message: {}", msg);
-    buffer.consume(buffer.size());
-
-    ws.async_read(buffer, beast::bind_front_handler(&session::on_read, shared_from_this()));
-}
-
-void session::send(const std::shared_ptr<const std::string>& ss)
-{
-    spdlog::info("Attempting to send {}", *ss);
-    // post our work to the strand, this ensures
-    // that members of 'this' will not be accessed concurrently
-    net::post(ws.get_executor(), beast::bind_front_handler(&session::on_send, shared_from_this(), ss));
-}
-
-
-
-void session::on_send(const std::shared_ptr<const std::string>& ss)
-{
-    spdlog::info("on_send()");
-    queue.push_back(ss);
-
-    // check if we're already writing
-    if (queue.size() > 1) {
-        spdlog::info("Already writing, so skip.");
-        return;
-    }
-
-    // since we are not writing, send this immediately
-    spdlog::info("We are not writing, so send this message now!");
-    ws.async_write(net::buffer(*queue.front()),
-      beast::bind_front_handler(&session::on_write, shared_from_this()));
-}
-
-void session::on_write(beast::error_code ec, std::size_t)
-{
-    if (ec) return fail(ec, "write");
-    spdlog::info("on_write");
-
-    // remove the string from the queue
-    queue.erase(queue.begin());
-
-    // send the message if any
-    if (!queue.empty()) {
-        ws.async_write(net::buffer(*queue.front()),
-          beast::bind_front_handler(&session::on_write, shared_from_this()));
-    }
-}
 
 void session::setHost(std::string hst)
 {
@@ -98,10 +40,100 @@ void session::setPort(std::string prt)
     this->port = std::move(prt);
 }
 
-void session::connect()
+void session::send(const std::shared_ptr<const std::string>& ss)
 {
-    resolver.async_resolve(this->host, this->port, beast::bind_front_handler(&session::on_resolve, shared_from_this()));
+    spdlog::debug("Attempting to send {}", *ss);
+    // post our work to the strand, this ensures
+    // that members of 'this' will not be accessed concurrently
+    net::post(ws.get_executor(), beast::bind_front_handler(&session::on_send, shared_from_this(), ss));
 }
+
+void session::on_send(const std::shared_ptr<const std::string>& ss)
+{
+    spdlog::debug("Sending message");
+    queue.push_back(ss);
+
+    // check if we're already writing
+    if (queue.size() > 1) {
+        return;
+    }
+
+    // since we are not writing, send this immediately
+    ws.async_write(net::buffer(*queue.front()),
+                   beast::bind_front_handler(&session::on_write, shared_from_this()));
+}
+
+void session::on_read(beast::error_code ec, std::size_t)
+{
+    if (ec) return fail(ec, "read");
+    spdlog::debug("Read incoming message.");
+
+    const std::string msg = beast::buffers_to_string(buffer.data());
+    spdlog::info("Received message: {}", msg);
+    buffer.consume(buffer.size());
+
+    ws.async_read(buffer, beast::bind_front_handler(&session::on_read, shared_from_this()));
+}
+
+void session::on_write(beast::error_code ec, std::size_t)
+{
+    if (ec) return fail(ec, "write");
+    spdlog::debug("Write to buffer successful");
+
+    // remove the string from the queue
+    queue.erase(queue.begin());
+
+    // send the message if any
+    if (!queue.empty()) {
+        ws.async_write(net::buffer(*queue.front()),
+          beast::bind_front_handler(&session::on_write, shared_from_this()));
+    }
+}
+
+void session::on_resolve(beast::error_code ec, tcp::resolver::results_type results)
+{
+    if (ec) return fail(ec, "resolve");
+    spdlog::debug("Resolve successful.");
+    beast::get_lowest_layer(ws).expires_after(std::chrono::seconds(30));
+
+    beast::get_lowest_layer(ws).async_connect(
+      results,
+      beast::bind_front_handler(&session::on_connect, shared_from_this()) );
+}
+
+void session::on_connect(beast::error_code ec, tcp::resolver::results_type::endpoint_type ep)
+{
+    if (ec) return fail(ec, "connect");
+    spdlog::debug("Connected to server.");
+
+    // turn off the timeout on the tcp_stream because
+    // the websocket stream has its own timeout system
+    beast::get_lowest_layer(ws).expires_never();
+
+    // set suggested timeout settings for the websocket
+    ws.set_option(websocket::stream_base::timeout::suggested(beast::role_type::client));
+
+    // set a decorator to change the User-Agent of the handshake
+    ws.set_option(websocket::stream_base::decorator(
+      [](websocket::request_type& req) {
+             req.set(http::field::user_agent, std::string(BOOST_BEAST_VERSION_STRING) + " chat-client");
+      }));
+
+    // update the host string. This will provide the value of the
+    // host HTTP header during the websocket handshake
+    // the guide references: https://tools.ietf.org/html/rfc7230#section-5.4
+    host += ':' + std::to_string(ep.port());
+
+    // now do the handshake
+    ws.async_handshake(host, "/", beast::bind_front_handler(&session::on_handshake, shared_from_this()));
+}
+
+void session::on_handshake(beast::error_code ec)
+{
+    if (ec) return fail(ec, "handshake");
+    spdlog::debug("Handshake successful.");
+}
+
 
 } /// Client
 namespace beast = boost::beast;         // from <boost/beast.hpp>
@@ -113,28 +145,39 @@ int main()
 {
     using namespace std::chrono_literals;
     using work_guard_type = boost::asio::executor_work_guard<boost::asio::io_context::executor_type>;
+
+    /// hardcoded for temporary testing
     const auto host = "0.0.0.0";
     const auto port = "8080";
-    const auto text = "new message";
 
+    /// everything will eventually be a message object
+    /// convert from Message to a json string
     const Message msg = {"Drue", "Sam", "hello"};
+    const nlohmann::json msgJson = to_json(msg);
+    const std::string msgJsonStr = msgJson.dump();
 
+    /// main io context
     net::io_context ioc;
+
+    /// work guard prevents ioc from ending too early
     work_guard_type workGuard(ioc.get_executor());
 
+    /// now we create a session!
     auto wsSession = std::make_shared<Client::session>(ioc); //->run(host, port, msg);
     wsSession->setHost(host);
     wsSession->setPort(port);
     wsSession->connect();
-    wsSession->send(std::make_shared<std::string>("Hello world."));
 
-    std::thread watchdog( [&] {
+    /// this thread sends the same periodically
+    /// used to test keeping a websocket alive while sending multiple messages
+    std::thread messageRepeater( [&] {
         while (true) {
             std::this_thread::sleep_for(10s);
-            wsSession->send(std::make_shared<std::string>("Hello"));
+            wsSession->send(std::make_shared<std::string>(msgJsonStr));
         }
     });
 
+    /// start our work
     ioc.run();
     return 0;
 }
