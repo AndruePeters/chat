5b8e3cf25897b6a964c94aa626e4600eb32a3bf1
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2021-07-31 20:30:24 -0400

    separated events from websocket.h

--- include/server_utilities/events.h
+++ include/server_utilities/events.h
@@ -5,4 +5,26 @@
 #ifndef CHAT_EVENTS_H
 #define CHAT_EVENTS_H
 
-#endif //CHAT_EVENTS_H
+#include <string>
+#include <boost/beast/core/error.hpp>
+#include <boost/beast/websocket/error.hpp>
+
+struct OpenEvent {
+
+};
+
+struct MessageEvent {
+    std::string data;
+};
+
+struct CloseEvent {
+    int reason = 0;
+    int code = 0;
+};
+
+struct ErrorEvent {
+    boost::beast::error_code ec = boost::beast::websocket::error::closed;
+    std::string what { };
+};
+
+#endif    //CHAT_EVENTS_H
